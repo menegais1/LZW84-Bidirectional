497dba7379fa852b1f48e53089154d5d01c59b8e
AspOfRodrigues <h.rodrigues2907@gmail.com>
2019-06-26 15:54:09 -0300

    Decode implemented, need to polish code, add file support, change from linked list to array for performance and manage memory better

--- lzw84bidirectional.c
+++ lzw84bidirectional.c
@@ -25,7 +25,7 @@ typedef struct out
 } Out;
 
 #define EOF_CODE 0
-
+#define MALLOC_SIZE 1000
 void addDictEntry(Dict *dict, char *string, int code)
 {
 	if (dict == NULL)
@@ -61,14 +61,16 @@ void initDict(Dict *dict)
 	dict->num = 0;
 	dict->root = NULL;
 	char i = 0;
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 0; i < 127; i++)
 	{
 		char c[2];
 		c[0] = i;
 		c[1] = 0;
 		addDictEntry(dict, c, -1);
 	}
+	
 	addDictEntry(dict, 0, EOF_CODE);
+
 }
 
 void removeNode(Entry *node)
@@ -126,14 +128,14 @@ void printOutput(Out **output, int size)
 	printf(")\n");
 }
 
-int hasEntryString(Dict *dict, char *text)
+int getEntryString(Dict *dict, char *text)
 {
 	if (dict == NULL)
 		return 0;
 	Entry *node = dict->root;
-	while (node != NULL && node->code != EOF_CODE)
+	while (node != NULL)
 	{
-		if (strcmp(node->string, text) == 0)
+		if (node->code != EOF_CODE && strcmp(node->string, text) == 0)
 		{
 			return node->code;
 		}
@@ -142,7 +144,7 @@ int hasEntryString(Dict *dict, char *text)
 	return 0;
 }
 
-char* hasEntryIndex(Dict *dict, int index)
+char* getEntryIndex(Dict *dict, int index)
 {
 	if (dict == NULL)
 		return 0;
@@ -158,76 +160,127 @@ char* hasEntryIndex(Dict *dict, int index)
 	return NULL;
 }
 
-char *encode(Dict *dict, char *text)
+void concat(char* dest,char* src1, char* src2){
+	strcpy(dest,src1);
+	strcat(dest,src2);
+}
+
+void concatCharOnString(char* src, char dest){
+	char* tmp = malloc(2);
+	tmp[0] = dest;
+	tmp[1] = 0;
+	strcat(src,tmp);
+	free(tmp);
+}
+void setString(char* dest, char*src){
+	strcpy(dest, src);
+	
+}
+
+void setCharOnString(char*dest, char src){
+	dest[0] = src;
+	dest[1] = 0;
+}
+
+Out* initOut(int code,int flag){
+	Out* out = (Out *)malloc(sizeof(Out));
+	out->index = code;
+	out->flag = 0;
+	return out;
+}
+
+Out** encode(Dict *dict, char *text)
 {
-	char *prefix = malloc(100);
-	char *current = malloc(100);
-	char *prefixCurrent = malloc(100);
-	Out *output[100];
-	prefix[0] = text[0];
-	prefix[1] = 0;
-	current[0] = 0;
+	char *prefix = malloc(MALLOC_SIZE);
+	char *currentChar = malloc(MALLOC_SIZE);
+	char *word = malloc(MALLOC_SIZE);
+	Out** output = (Out**) malloc(sizeof(Out*) * MALLOC_SIZE);
 	int code = 0;
 	int cont = 0;
-	while (*text != 0)
+	prefix[0] = text[0];
+	setCharOnString(prefix, *text);
+	
+	currentChar[0] =0;
+	while(*text != 0)
 	{
-		strcpy(prefixCurrent, prefix);
-		strcat(prefixCurrent, current);
-		int tempCode = hasEntryString(dict, prefixCurrent);
+		concat(word,prefix,currentChar);
+		int tempCode = getEntryString(dict, word);
 		if (tempCode)
 		{
-			strcpy(prefix, prefixCurrent);
+			setString(prefix, word);
 			text++;
-			current[0] = *text;
-			current[1] = 0;
+			setCharOnString(currentChar,*text);
 			code = tempCode;
+			if(*currentChar == 0){
+			output[cont] = initOut(code,0);
+			cont++;
+			break;
+			}
 		}
 		else
 		{
-			addDictEntry(dict, prefixCurrent, -1);
-			strcpy(prefix, current);
-			current[0] = 0;
-			output[cont] = (Out *)malloc(sizeof(Out));
-			output[cont]->index = code;
-			output[cont]->flag = 0;
+			addDictEntry(dict, word, -1);
+			setString(prefix, currentChar);
+			setCharOnString(currentChar, 0);
+			output[cont] = initOut(code,0);
 			cont++;
 		}
+		
 	}
 
+	output[cont] = initOut(EOF_CODE,0);	
+	cont++;
 	printDict(dict);
 	printOutput(output, cont);
+	return output;
 }
 
 void decode(Dict *dict, Out **input)
 {
 	int oldCode;
 	int code = (*input)->index;
-	char* word = hasEntryIndex(dict,code);
-	char* oldWord = word;
-	char* output = malloc(100);
+	char* word = getEntryIndex(dict,code);
+	char* oldWord = malloc(MALLOC_SIZE);
+	setString(oldWord, word);
+	char* output = malloc(MALLOC_SIZE);
+	int i;
+	for(i=0;i<MALLOC_SIZE;i++){
+		output[i] = 0;
+	}
 	strcat(output, word);
+	input++;
 	while ((*input)->index != EOF_CODE)
 	{
 		code = (*input)->index;
-		word = hasEntryIndex(dict,code);
+
+		word = getEntryIndex(dict,code);
 		if(word != NULL){
 			strcat(output, word);
-			char* temp = malloc(100);
+			concatCharOnString(oldWord, word[0]);
+			addDictEntry(dict,oldWord,-1);
+			setString(oldWord,word);
 		}else{
-
+			concatCharOnString(oldWord, oldWord[0]);
+			strcat(output, oldWord);
+			addDictEntry(dict,oldWord,-1);
+			printDict(dict);
 		}
+		input++;
 	}
+
+	printf("%s", output);
 }
 
 int main()
 {
-	char *text = "aabcabacb";
+	char *text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis elementum nibh. Maecenas fermentum volutpat turpis, id sollicitudin tellus vestibulum at. Mauris molestie turpis a venenatis malesuada. Cras ut laoreet enim. Nam id maximus quam. Mauris tempor blandit nunc id tempus. Fusce tincidunt velit nec enim porttitor ornare. Aliquam id vulputate nisl, eu tempor est. In ultrices et lacus sed lobortis. Suspendisse ligula dui, posuere molestie ultricies vitae, ultrices eget lorem. Donec sodales pharetra finibus. Duis vitae nisi eget ante facilisis blandit et vel arcu. Donec a facilisis augue. Sed auctor aliquet lectus sit amet consectetur. Integer nibh dui, pretium.";
 	Dict *dict = (Dict *)malloc(sizeof(Dict));
 	initDict(dict);
 	printDict(dict);
 
-	encode(dict, text);
-	printDict(dict);
-
+	Out** output = encode(dict, text);
+	clearDict(dict);
+	initDict(dict);
+	decode(dict,output);
 	return 0;
 }
